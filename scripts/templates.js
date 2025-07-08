3c1312aadf742c2dd79fa9c6ef708ac569a70cb2
Daniel Luzius <danielluzius@mail.de>
2025-07-08 22:35:53 +0200

    added icons as buttons and changed note background

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,11 +1,11 @@
 function getNoteTemplate(indexNote) {
   return `
   <div class="note">
-    <h3>${notesTitle[indexNote]} </h3>
+    <h3>${notesTitle[indexNote]}</h3>
     <p>${notes[indexNote]}</p>
     <div class="note-btn-style">
-      <button class="note-btn" onclick="pushNoteToArchive(${indexNote})">A</button>
-      <button class="note-btn" onclick="pushNoteToTrash(${indexNote})">X</button>
+      <img src="./assets/img/icons/icon-archive.png" class="note-icon" alt="Archivieren" onclick="pushNoteToArchive(${indexNote})">
+      <img src="./assets/img/icons/icon-trash.png" class="note-icon" alt="In den Papierkorb" onclick="pushNoteToTrash(${indexNote})">
     </div>
   </div>`;
 }
@@ -13,11 +13,11 @@ function getNoteTemplate(indexNote) {
 function getArchivedNoteTemplate(indexArchivedNote) {
   return `
   <div class="note">
-    <h3>${archivedNotesTitle[indexArchivedNote]} </h3>
+    <h3>${archivedNotesTitle[indexArchivedNote]}</h3>
     <p>${archivedNotes[indexArchivedNote]}</p>
     <div class="note-btn-style">
-      <button class="note-btn" onclick="pushArchivedNoteToNotes(${indexArchivedNote})">N</button>
-      <button class="note-btn" onclick="pushArchivedNoteToTrash(${indexArchivedNote})">X</button>
+      <img src="./assets/img/icons/icon-back.png" class="note-icon" alt="Zurück zu den Notizen" onclick="pushArchivedNoteToNotes(${indexArchivedNote})">
+      <img src="./assets/img/icons/icon-trash.png" class="note-icon" alt="In den Papierkorb" onclick="pushArchivedNoteToTrash(${indexArchivedNote})">
     </div>
   </div>`;
 }
@@ -25,11 +25,11 @@ function getArchivedNoteTemplate(indexArchivedNote) {
 function getTrashedNoteTemplate(indexTrashedNote) {
   return `
   <div class="note">
-    <h3>${trashedNotesTitle[indexTrashedNote]} </h3>
+    <h3>${trashedNotesTitle[indexTrashedNote]}</h3>
     <p>${trashedNotes[indexTrashedNote]}</p>
     <div class="note-btn-style">
-      <button class="note-btn" onclick="pushRestoredNoteToNotes(${indexTrashedNote})">N</button>
-      <button class="note-btn" onclick="deleteNote(${indexTrashedNote})">X</button>
+      <img src="./assets/img/icons/icon-back.png" class="note-icon" alt="Wiederherstellen" onclick="pushRestoredNoteToNotes(${indexTrashedNote})">
+      <img src="./assets/img/icons/icon-delete.png" class="note-icon" alt="Endgültig löschen" onclick="deleteNote(${indexTrashedNote})">
     </div>
   </div>`;
 }
